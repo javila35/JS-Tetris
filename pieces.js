efd18645c57d9bd0b4a6993872596f9b68f91bdd
clheard54 <clarion.heard@gmail.com>
2020-03-23 16:37:56 -0700

    basic tetrad functions

--- pieces.js
+++ pieces.js
@@ -151,3 +151,49 @@ const T = [
             [0,1,0]
         ]
     ]
+
+    class Piece{
+        constructor(tetrad, color){
+            this.tetrad = tetrad;
+            this.color = color;
+            this.stage = 0;
+            this.activeTetrad = this.tetrad[this.stage]
+            this.x = 4;
+            this.y = 2;
+        }
+    
+        colorTetrad(color){
+            for (r=0; r<this.activeTetrad.length; r++){
+                for (c=0; c<this.activeTetrad.length; c++){
+                    if (this.activeTetrad[r][c]==1){
+                        drawSquare(this.x + c, this.y + r, color)
+                    }
+                }
+            }
+        }
+    
+        drawTetrad(){
+            this.colorTetrad(this.color);
+        }
+    
+        deleteTetrad(){
+            this.colorTetrad(white)
+        }
+    
+        moveTetradDown(){
+            this.deleteTetrad()
+            this.y++
+            this.drawTetrad()   
+            }
+    
+        lockTetrad(){
+            if (this.y>=19){
+                this.locked = true;
+            }
+        }
+    }
+    
+    
+    let ess = new Piece(S, "blue");
+    ess.drawTetrad()
+    
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const BLOCKS_URL = "http://localhost/api/v1/blocks"
 
 document.addEventListener("DOMContentLoaded", function(){
-let currentPiece = null;
 
 //Select Random Piece
     function getRandomPiece(){
@@ -14,53 +13,44 @@ let currentPiece = null;
 
     function pickPiece(piecesArray){
         let id = Math.floor(Math.random()*7)
-        let shape = piecesArray.find(id)
+        let block = piecesArray[id]
+        let letter = eval(block.shape)
+        console.log(letter)
         currentPiece = shape
     }
 
 //Get currentPiece.s1 to show up on the screen
-
+//See board-set-up (drawTetrad/colorTetrad)
 
 
 //Normal falling piece function
-  function movePieceDown(piece){
-      setTimeout(function(){
-          let oldPosition = parseInt(piece.style.top) || 1;
-          let newPosition = oldPosition + 5
-
-          if (oldPosition < 570){
-            piece.style.top = newPosition + "px";
-          }
-          movePieceDown(piece)
-        }, 50)    
-      }
-})
+  
 
 //Move piece left
-function movePieceLeft(piece) {
-    let rightEdge = piece.style.right.replace("px", "");
+function moveTetradLeft(tetrad) {
+    let rightEdge = tetrad.style.right.replace("px", "");
     let right = parseInt(rightNumber, 10);
    
     if (right < 300) {
-      piece.style.right = `${right + 30}px`;
+      tetrad.style.right = `${right + 30}px`;
     }
   }
 
 
-//Move piece right
-function movePieceRight(piece) {
-    let rightEdge = piece.style.right.replace("px", "");
+//Move tetrad right
+function moveTetradRight(tetrad) {
+    let rightEdge = tetrad.style.right.replace("px", "");
     let right = parseInt(rightNumber, 10);
    
     if (right >= 30) {
-      piece.style.right = `${right - 30}px`;
+      tetrad.style.right = `${right - 30}px`;
     }
   }
 
 //Arrow left function
 document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowLeft") {
-      movePieceLeft(currentPiece);
+      ess.moveTetradLeft();
     }
   });
 
@@ -68,7 +58,14 @@ document.addEventListener("keydown", function(e) {
 //Arrow right function
 document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowRight") {
-      movePieceRight(currentPiece);
+      ess.moveTetradRight();
     }
   });
 
+  //Arrow down function
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowDown") {
+      ess.moveTetradDown();
+    }
+  })
+})

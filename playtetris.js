8ce7104cfe52fc7f83dac77e2783e387d69a4b2c
clheard54 <clarion.heard@gmail.com>
2020-03-24 10:25:13 -0700

    tuesday morning

--- playtetris.js
+++ playtetris.js
@@ -2,31 +2,12 @@ const BLOCKS_URL = "http://localhost/api/v1/blocks"
 
 document.addEventListener("DOMContentLoaded", function(){
 
-//Select Random Piece
-    function getRandomPiece(){
-        fetch(BLOCKS_URL)
-        .then(resp => resp.json())
-        .then(data => {
-            pickPiece(data)
-        })
-    }
-
-    function pickPiece(piecesArray){
-        let id = Math.floor(Math.random()*7)
-        let block = piecesArray[id]
-        let letter = eval(block.shape)
-        console.log(letter)
-        currentPiece = shape
-    }
-
-    // currentPiece
-
 //Get currentPiece to show up on the screen
-//See board-set-up (drawTetrad/colorTetrad)
-
+// SEE BOARD-SET-UP (colorTetrad, drawTatrad)
 
 //Normal falling piece function
 // SEE BOARD SET-UP
+
 
 //Arrow left function
 document.addEventListener("keydown", function(e) {
